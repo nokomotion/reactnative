8cc633dae989014a3e66751b03896e67f71a8e9e
nokomotion <isaacgasparovich@hotmail.com>
2020-09-25 23:09:35 -0500

    primera version

---
+++ App.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text,Button,Alert,SafeAreaView, TextInput,ImageBackground } from 'react-native';
+
+const image = { uri: "https://reactjs.org/logo-og.png" };
+
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [kilometros, setkilometros] = useState('');
+
+  return (
+      <View style={estilo.container}>
+    <ImageBackground source={image} style={estilo.image}>
+      <Text style={estilo.text}>Inside</Text>
+    </ImageBackground>
+  
+  
+      
+        <Text>Nombre:</Text>
+
+        <TextInput
+          value={name}
+          onChangeText={(name) => setName(name)}
+          placeholder={'Ingresa tu nombre'}
+          style={estilo.input}
+        />
+
+        <View style={{height:30}}/>
+        <Text>Kilometros:</Text>
+
+         <TextInput
+          value={kilometros} keyboardType='numeric'
+          onChangeText={(kilometros) => setkilometros(kilometros)}
+          placeholder={'Ingresa los kilometros'}
+          style={estilo.input}
+        />
+      <Button
+        title="suma"
+        onPress={() => Alert.alert(1+2)}
+      />
+        <Text style={{color: 'blue'}}>{kilometros}</Text>
+      </View>
+    
+  );
+};
+
+
+const estilo =StyleSheet.create({
+  container:{
+    flex:1,    
+    justifyContent:'center'
+  },
+  subcontaier:{
+      height:50,
+      width:'100%',
+      flexDirection:"row",
+      paddingHorizontal: 10
+
+  },
+  texto:{
+    
+    borderBottomWidth:1,
+  
+  
+
+  },
+  input: {
+  height: 70,
+  backgroundColor: '#ffffff',
+  paddingLeft: 15,
+  paddingRight: 15,
+  borderBottomWidth:2
+},
+image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  btn:{
+    width:50,
+    height:50,
+    borderRadius:5,
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor:"red"
+  },
+  btntxt:{
+    fontSize:25,
+    color:"blue",
+    fontWeight:'bold'
+  },
+  counterContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+  contador:{
+    color:'white',
+    fontSize:50
+  }
+  
+  });
+
+export default App;
